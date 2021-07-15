08a6ff60b1b345015f389cb35a9c745b2fbd870a
Casey K <tehcnicolour@gmail.com>
2021-07-14 19:38:23 -0700

    init

---
+++ web/src/components/Header/Navigation/Navigation.tsx
@@ -0,0 +1,51 @@
+// Navigation
+
+// ___________________________________________________________________
+
+import React from 'react'
+import { Link } from 'gatsby'
+import { useColorMode } from 'theme-ui'
+import theme from '../../../gatsby-plugin-theme-ui'
+import * as S from './styles.scss'
+
+// ___________________________________________________________________
+
+const Navigation = () => {
+  return (
+    <S.Nav>
+      {data.map((item, idx) => (
+        <Link
+          to={item.link}
+          activeClassName="active"
+          partiallyActive={true}
+          key={idx}
+        >
+          {item.name}
+        </Link>
+      ))}
+    </S.Nav>
+  )
+}
+
+export default Navigation
+
+// ___________________________________________________________________
+
+const data = [
+  {
+    name: 'Intro',
+    link: '/intro'
+  },
+  {
+    name: 'Speakers',
+    link: '/speakers'
+  },
+  {
+    name: 'Features',
+    link: '/features'
+  },
+  {
+    name: 'RSVP',
+    link: '/rsvp'
+  },
+]
---
+++ web/src/templates/HomePage/HomePage.tsx
@@ -0,0 +1,89 @@
+// HomePage:
+
+// ___________________________________________________________________
+
+import React from 'react'
+
+// Theme + ui
+import theme from '../../gatsby-plugin-theme-ui'
+import * as S from './styles.scss'
+import { Box, Button, Flex, Heading, Text } from 'theme-ui'
+
+// Components
+import Section from '../../components/Section'
+
+// ___________________________________________________________________
+
+const HomePage: React.FC = () => {
+  return (
+    <S.HomePage>
+      <Section>
+        <Heading as="h1" mb={2}>
+          The dWeb Summit{' '}
+          <Text as="span" sx={{ fontFamily: `display` }}>
+            2021
+          </Text>
+        </Heading>
+        <Text as="p" mb={6}>
+          Golem generated the quick testnet for lots of circulating supply! When
+          Nexo thinking many constant private key, Dash based on a genesis
+          block! Maker left few confirmation at a IPO.
+        </Text>
+
+        <Flex mb={6}>
+          <Button mr={2} variant="primary">
+            Buy Tickets
+          </Button>
+          <Button mr={2} bg="secondary" color="black" variant="primary">
+            Buy Tickets
+          </Button>
+          <Button variant="secondary">Buy Tickets</Button>
+        </Flex>
+
+        <Heading as="h2" mb={2}>
+          Bitcoin Cash identified lots of mainnet.
+        </Heading>
+        <Heading as="h3" mb={2}>
+          Bitcoin Cash identified lots of mainnet.
+        </Heading>
+        <Heading as="h4" mb={2}>
+          Bitcoin Cash identified lots of mainnet.
+        </Heading>
+        <Heading as="h5" mb={2}>
+          Bitcoin Cash identified lots of mainnet.
+        </Heading>
+        <Heading as="h6">Bitcoin Cash identified lots of mainnet.</Heading>
+      </Section>
+
+      <Section bg="text" color="gray">
+        <Heading as="h1" mb={2}>
+          The dWeb Summit{' '}
+          <Text as="span" sx={{ fontFamily: `display` }}>
+            2021
+          </Text>
+        </Heading>
+        <Text as="p" mb={6}>
+          Golem generated the quick testnet for lots of circulating supply! When
+          Nexo thinking many constant private key, Dash based on a genesis
+          block! Maker left few confirmation at a IPO.
+        </Text>
+      </Section>
+
+      <Section bg="secondary">
+        <Heading as="h1" mb={2}>
+          The dWeb Summit{' '}
+          <Text as="span" sx={{ fontFamily: `display` }}>
+            2021
+          </Text>
+        </Heading>
+        <Text as="p" mb={6}>
+          Golem generated the quick testnet for lots of circulating supply! When
+          Nexo thinking many constant private key, Dash based on a genesis
+          block! Maker left few confirmation at a IPO.
+        </Text>
+      </Section>
+    </S.HomePage>
+  )
+}
+
+export default HomePage
